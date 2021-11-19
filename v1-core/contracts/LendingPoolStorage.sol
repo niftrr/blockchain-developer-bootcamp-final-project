d7b1362c7e0f64c9dcf0d65e4e69973ffa325096
niftrr <niftrr@protonmail.com>
2021-11-19 17:15:55 +0100

    Updated Lending Pool Storage contract to include NatSpec

--- v1-core/contracts/LendingPoolStorage.sol
+++ v1-core/contracts/LendingPoolStorage.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 import { DataTypes } from './libraries/DataTypes.sol';
 import { ReserveLogic } from './libraries/ReserveLogic.sol';
 
+/// @title Lending Pool Storage contract for maintaining storage (WIP).
+/// @author Niftrr
+/// @notice Separates storage from the Lending Pool contract.
+/// @dev This is not yet implmented.
 contract LendingPoolStorage {
     using ReserveLogic for DataTypes.ReserveData;
     // using UserConfiguration for DataTypes.UserConfigurationMap;
